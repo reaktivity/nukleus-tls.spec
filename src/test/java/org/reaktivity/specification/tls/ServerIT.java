1a3c22fbac8b9b3b6731f27bb81c4951a5212ac7
John Fallows <john.fallows@kaazing.com>
2017-06-08 23:54:43 -0700

    Initial implementation

--- src/test/java/org/reaktivity/specification/tls/ServerIT.java
+++ src/test/java/org/reaktivity/specification/tls/ServerIT.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package org.reaktivity.specification.nukleus.tls.stream;
+package org.reaktivity.specification.tls;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
@@ -23,7 +23,6 @@ import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
-import org.kaazing.k3po.junit.annotation.ScriptProperty;
 import org.kaazing.k3po.junit.annotation.Specification;
 import org.kaazing.k3po.junit.rules.K3poRule;
 import org.reaktivity.specification.nukleus.NukleusRule;
@@ -31,7 +30,7 @@ import org.reaktivity.specification.nukleus.NukleusRule;
 public class ServerIT
 {
     private final K3poRule k3po = new K3poRule()
-        .addScriptRoot("scripts", "org/reaktivity/specification/nukleus/tls/streams");
+        .addScriptRoot("scripts", "org/reaktivity/specification/tls");
 
     private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
 
@@ -45,7 +44,7 @@ public class ServerIT
     @Specification({
         "${scripts}/connection.established/client",
         "${scripts}/connection.established/server"})
-    @ScriptProperty("serverConnect \"nukleus://tls/streams/source\"")
+    //@ScriptProperty("clientAccept \"nukleus://target/streams/tls#source\"")
     public void connectionEstablished() throws Exception
     {
         k3po.start();
